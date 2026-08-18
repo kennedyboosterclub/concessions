91b2b69e5dbdc32c747b1aaeccae37cff182f937
kennedyboosterclub <kennedy.booster.treasurer@gmail.com>
2026-08-18 17:47:29 -0500

    Update concession menu from menu editor

--- menu-data.js
+++ menu-data.js
@@ -1,40 +1,115 @@
 window.KENNEDY_MENU = {
-  title: "KENNEDY",
-  subtitle: "CONCESSIONS",
-  tagline: "GRAB A SNACK. CHEER LOUD. GO EAGLES!",
-  footer: "ALL PROCEEDS SUPPORT OUR FOOTBALL ATHLETES!",
-  hotFood: [
-    {name:"HOT DOG", price:"3", image:"assets/hotdog.png"},
-    {name:"CHIPS", price:"2", image:"assets/chips.png"},
-    {name:"PIZZA SLICE", price:"5", image:"assets/pizza.png"},
-    {name:"NACHOS & CHEESE", price:"4", image:"assets/nachos.png"},
-    {name:"POPCORN", price:"2", image:"assets/popcorn.png"}
+  "title": "KENNEDY",
+  "subtitle": "CONCESSIONS",
+  "tagline": "GRAB A SNACK. CHEER LOUD. GO EAGLES!",
+  "footer": "ALL PROCEEDS SUPPORT OUR FOOTBALL ATHLETES!",
+  "hotFood": [
+    {
+      "name": "HOT DOG",
+      "price": "3",
+      "image": "assets/hotdog.png"
+    },
+    {
+      "name": "CHIPS",
+      "price": "2",
+      "image": "assets/chips.png"
+    },
+    {
+      "name": "PIZZA SLICE",
+      "price": "5",
+      "image": "assets/pizza.png"
+    },
+    {
+      "name": "NACHOS & CHEESE",
+      "price": "4",
+      "image": "assets/nachos.png"
+    },
+    {
+      "name": "POPCORN",
+      "price": "2",
+      "image": "assets/popcorn.png"
+    }
   ],
-  drinks: [
-    {name:"WATER", price:"1", image:"assets/water.png"},
-    {name:"BUBBLR", price:"4", image:"assets/bubblr.png"},
-    {name:"GATORADE", price:"3", image:"assets/gatorade.png"},
-    {name:"SODA (BOTTLES)", price:"3", image:"assets/sodas.png", description:"Coke, Diet Coke, Sprite, Mountain Dew, Orange Crush, Dr Pepper"}
+  "drinks": [
+    {
+      "name": "WATER",
+      "price": "1",
+      "image": "assets/water.png"
+    },
+    {
+      "name": "BUBBLR",
+      "price": "4",
+      "image": "assets/bubblr.png"
+    },
+    {
+      "name": "GATORADE",
+      "price": "3",
+      "image": "assets/gatorade.png"
+    },
+    {
+      "name": "SODA (BOTTLES)",
+      "price": "3",
+      "image": "assets/sodas.png",
+      "description": "Coke, Diet Coke, Sprite, Mountain Dew, Orange Crush, Dr Pepper"
+    }
   ],
-  snacks: [
-    {name:"UNCRUSTABLES", price:"2", image:"assets/uncrustables.png"},
-    {name:"RICE CRISPY BARS", price:"1", image:"assets/ricecrispy.png"},
-    {name:"GRANOLA BARS", price:"1", image:"assets/granola.png"},
-    {name:"WHOLE PICKLE", price:"2", image:"assets/pickle.png"}
+  "snacks": [
+    {
+      "name": "UNCRUSTABLES",
+      "price": "2",
+      "image": "assets/uncrustables.png"
+    },
+    {
+      "name": "RICE CRISPY BARS",
+      "price": "1",
+      "image": "assets/ricecrispy.png"
+    },
+    {
+      "name": "GRANOLA BARS",
+      "price": "1",
+      "image": "assets/granola.png"
+    },
+    {
+      "name": "WHOLE PICKLE",
+      "price": "2",
+      "image": "assets/pickle.png"
+    }
   ],
-  candy: [
-    {name:"CANDY BARS", price:"2", image:"assets/candy.png"},
-    {name:"RING POPS", price:"2", image:"assets/candy.png"},
-    {name:"COTTON CANDY", price:"4", image:"assets/candy.png"}
+  "candy": [
+    {
+      "name": "CANDY BARS",
+      "price": "2",
+      "image": "assets/candy.png"
+    },
+    {
+      "name": "RING POPS",
+      "price": "2",
+      "image": "assets/candy.png"
+    },
+    {
+      "name": "COTTON CANDY",
+      "price": "4",
+      "image": "assets/candy.png"
+    }
   ],
-  meals: [
-    {name:"HOT DOG MEAL", price:"6", description:"Hot dog, chips & Gatorade or soda", image:"assets/hotdog.png"},
-    {name:"PIZZA MEAL", price:"8", description:"Pizza slice, chips & Gatorade or soda", image:"assets/pizza.png"}
+  "meals": [
+    {
+      "name": "HOT DOG MEAL",
+      "price": "6",
+      "description": "Hot dog, chips & Gatorade or soda",
+      "image": "assets/hotdog.png"
+    },
+    {
+      "name": "PIZZA MEAL",
+      "price": "9",
+      "description": "Pizza slice, chips & Gatorade or soda",
+      "image": "assets/pizza.png"
+    }
   ],
-  showPayments: true,
-  payments: "CASH • CARD • VENMO",
-  paymentQr: "",
-  showProgram: true,
-  programText: "SCAN TO VIEW TODAY'S DIGITAL GAME DAY PROGRAM!",
-  programQr: ""
+  "showPayments": true,
+  "payments": "CASH • CARD • VENMO",
+  "paymentQr": "",
+  "showProgram": true,
+  "programText": "SCAN TO VIEW TODAY'S DIGITAL GAME DAY PROGRAM!",
+  "programQr": ""
 };
